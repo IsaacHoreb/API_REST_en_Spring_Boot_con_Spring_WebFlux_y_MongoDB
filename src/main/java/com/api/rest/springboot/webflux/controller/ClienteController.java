907fb4d10fdd7a8b29443531eb857f43423a0407
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-08-12 00:18:52 -0600

    Metodo PostMapping

---
+++ src/main/java/com/api/rest/springboot/webflux/controller/ClienteController.java
@@ -0,0 +1,46 @@
+//Quinto -- *Pero ya debimos haber ingresado los datos en el archivos <<properties>>
+package com.api.rest.springboot.webflux.controller;
+
+import com.api.rest.springboot.webflux.document.Cliente;
+import com.api.rest.springboot.webflux.services.ClientesServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+import java.io.File;
+import java.net.URI;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteController {
+
+    @Autowired
+    private ClientesServices clientesServices;
+
+    //Esto es para guardar las fotos por medio
+    //de la ruta que ingresamos en el archivo <<properties>>
+    @Value("${config.uploads.path}")
+    private String path;
+
+    //Registrar cliente con foto
+    @PostMapping("/registrarClienteConFotos")
+    public Mono<ResponseEntity<Cliente>> registrarClienteConFoto(Cliente cliente, @RequestPart FilePart file) {
+
+        cliente.setFoto(UUID.randomUUID().
+                toString() + " - " + file.filename()
+                .replace(" ", "")
+                .replace(":", "")
+                .replace("//", ""));
+
+        return file.transferTo(new File(path + cliente.getFoto())).then(clientesServices.Save(cliente))
+                .map(c -> ResponseEntity.created(URI.create("/api/clientes".concat(c.getId())))
+                        .contentType(MediaType.APPLICATION_JSON_UTF8).body(c));
+    }
+
+
+}
