3c411d6b444cd27515bc13ce333dddd5cc38782c
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-08-12 16:45:01 -0600

    Metodo Post, para guardar un cliente

--- src/main/java/com/api/rest/springboot/webflux/controller/ClienteController.java
+++ src/main/java/com/api/rest/springboot/webflux/controller/ClienteController.java
@@ -6,20 +6,20 @@ import com.api.rest.springboot.webflux.services.ClientesServices;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.WebExchangeBindException;
+import org.springframework.web.server.WebExceptionHandler;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import javax.swing.*;
 import java.io.File;
 import java.net.URI;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -74,5 +74,39 @@ public class ClienteController {
                 .contentType(MediaType.APPLICATION_JSON_UTF8).body(c)).defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
-    
+    //Guardar detalles del cliente
+    @PostMapping
+    public Mono<ResponseEntity<Map<String, Object>>> guardarCliente(@Valid @RequestBody Mono<Cliente> monoCliente) {
+        Map<String, Object> respuesta = new HashMap<>();
+
+        return monoCliente.flatMap(cliente -> {
+            return clientesServices.Save(cliente).map(c -> {
+                respuesta.put("clientes", c);
+                respuesta.put("mensaje", "Cliente guardardo con éxito");
+                respuesta.put("timestamp", new Date());
+                return ResponseEntity.created(URI.create("/api/clientes/".concat(c.getId())))
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .body(respuesta);
+            });
+        }).onErrorResume(t -> {
+            if (t instanceof WebExchangeBindException) {
+                WebExchangeBindException bindException = (WebExchangeBindException) t;
+
+                List<String> errores = bindException.getFieldErrors()
+                        .stream()
+                        .map(fieldError -> "El campo : " + fieldError.getField() + " " + fieldError.getDefaultMessage())
+                        .collect(Collectors.toList());
+
+                respuesta.put("errores", errores);
+                respuesta.put("timestamp", new Date());
+                respuesta.put("status", HttpStatus.BAD_REQUEST.value());
+
+                return Mono.just(ResponseEntity.badRequest().body(respuesta));
+            } else {
+                // Manejar otros tipos de errores aquí si es necesario
+                return Mono.error(t);
+            }
+        });
+    }
 }
+
