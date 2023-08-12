ac136629fc257a98fe076fe472fbea6bb584b1e0
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-08-11 20:41:39 -0600

    Proyecto Limpio

---
+++ src/main/java/com/api/rest/springboot/webflux/ApiRestSpringbootWebfluxMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.api.rest.springboot.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSpringbootWebfluxMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSpringbootWebfluxMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/rest/springboot/webflux/ApiRestSpringbootWebfluxMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.rest.springboot.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpringbootWebfluxMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
